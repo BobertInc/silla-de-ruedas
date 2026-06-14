d8eb880ec0315885d7156b74238ee0488fb98a8f
Robert_Enc <140596327+BobertInc@users.noreply.github.com>
2026-06-13 19:36:14 -0500

    Update and rename esp32s3.ino to transmisor.ino

--- transmisor.ino
+++ transmisor.ino
@@ -2,20 +2,19 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define SDA_PIN 8
-#define SCL_PIN 9
-
+#define SDA_PIN 21
+#define SCL_PIN 22
 #define MPU_ADDR 0x68
 
 // ESP NOW DATA
-typedef struct {
+typedef struct __attribute__((packed)) {
   float x;
   float y;
 } ControlData;
 
 ControlData data;
 
-uint8_t receiverMac[] = {0x34, 0x5F, 0x45, 0xAA, 0x61, 0x3C};
+uint8_t receiverMac[] = {0x88, 0x13, 0xBF, 0x09, 0x49, 0xC8};
 
 float Total_angle_x = 0;
 float Total_angle_y = 0;
@@ -28,12 +27,13 @@ unsigned long previousTime;
 
 void setup() {
   Serial.begin(115200);
+  delay(500);
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
 
   if (esp_now_init() != ESP_OK) {
-    Serial.println("ESP NOW INdIT FAILED");
+    Serial.println("ESP NOW INIT FAILED");
     while (1);
   }
 
@@ -76,11 +76,13 @@ void setup() {
     Wire.endTransmission(false);
     Wire.requestFrom(MPU_ADDR, 4, true);
 
-    int16_t rawGyroX = (Wire.read() << 8) | Wire.read();
-    int16_t rawGyroY = (Wire.read() << 8) | Wire.read();
+    if(Wire.available() >= 4) {
+      int16_t rawGyroX = (Wire.read() << 8) | Wire.read();
+      int16_t rawGyroY = (Wire.read() << 8) | Wire.read();
 
-    gyroOffsetX += (float)rawGyroX / 131.0;
-    gyroOffsetY += (float)rawGyroY / 131.0;
+      gyroOffsetX += (float)rawGyroX / 131.0;
+      gyroOffsetY += (float)rawGyroY / 131.0;
+    }
 
     delay(5);
   }
@@ -101,9 +103,7 @@ void loop() {
 
   Wire.beginTransmission(MPU_ADDR);
   Wire.write(0x43);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_ADDR, 4, true);
-
+ 
   int16_t rawGyroX = (Wire.read() << 8) | Wire.read();
   int16_t rawGyroY = (Wire.read() << 8) | Wire.read();
 
@@ -124,7 +124,7 @@ void loop() {
   float accX = (float)rawAccX / 16384.0;
   float accY = (float)rawAccY / 16384.0;
   float accZ = (float)rawAccZ / 16384.0;
-
+  
   // ACCEL ANGLES
   float Acc_angle_x = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
   float Acc_angle_y = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
@@ -132,18 +132,18 @@ void loop() {
   // COMPLEMENTARY FILTER
   Total_angle_x = 0.98 * (Total_angle_x + gyroX * elapsedTime) + 0.02 * Acc_angle_x;
   Total_angle_y = 0.98 * (Total_angle_y + gyroY * elapsedTime) + 0.02 * Acc_angle_y;
-
+  
   // SEND DATA
-  data.x = Total_angle_x;
-  data.y = Total_angle_y;
+  data.x = constrain(Total_angle_x, -45.0, 45.0);
+  data.y = constrain(Total_angle_y, -45.0, 45.0);
   esp_err_t result = esp_now_send(receiverMac, (uint8_t*)&data, sizeof(data));
 
   // DEBUG
   Serial.print("X: ");
   Serial.print(data.x);
-  Serial.print(" Y: ");
+  Serial.print(" | Y: ");
   Serial.print(data.y);
-  Serial.print(" SEND: ");
+  Serial.print(" | SEND: ");
 
   if (result == ESP_OK) {
     Serial.println("OK");
@@ -151,5 +151,5 @@ void loop() {
     Serial.println("FAIL");
   }
 
-  delay(20);
+  delay(30);
 }
